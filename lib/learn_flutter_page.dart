fbb5df23866b34168c17b3d9e06768b922973399
LBF38 <96347823+LBF38@users.noreply.github.com>
2022-09-20 14:11:09 +0200

    Testing with items, views in Home page Playing around with different widgets
    End of the Beginner 1h Flutter MApp Tuto

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,88 +23,99 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           },
           icon: const Icon(Icons.arrow_back_ios),
         ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('Actions');
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
       ),
-      body: Column(
-        children: [
-          Image.asset("images/einstein.jpg"),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10.0),
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.blueGrey,
-            width: double.infinity,
-            child: const Center(
-              child: Text(
-                "This is a text widget",
-                style: TextStyle(
-                  color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("images/einstein.jpg"),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: isSwitch ? Colors.green : Colors.blue,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint("Elevated Button");
+              },
+              child: const Text("Elevated Button"),
             ),
-            onPressed: () {
-              debugPrint("Elevated Button");
-            },
-            child: const Text("Elevated Button"),
-          ),
-          OutlinedButton(
-            onPressed: () {
-              debugPrint("Outlined Button");
-            },
-            child: const Text("Outlined Button"),
-          ),
-          TextButton(
-            onPressed: () {
-              debugPrint("Text Button");
-            },
-            child: const Text("Text Button"),
-          ),
-          GestureDetector(
-            behavior: HitTestBehavior.opaque,
-            onTap: () {
-              debugPrint("This is the row");
-            },
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-                Text('Row Widget'),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-              ],
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("Outlined Button");
+              },
+              child: const Text("Outlined Button"),
             ),
-          ),
-          Switch(
-            value: isSwitch,
-            onChanged: (bool newBool) {
-              setState(() {
-                isSwitch = newBool;
-              });
-            },
-          ),
-          Checkbox(
-            value: isCheckBox,
-            onChanged: (bool? newBool) {
-              setState(() {
-                isCheckBox = newBool;
-              });
-            },
-          ),
-        ],
+            TextButton(
+              onPressed: () {
+                debugPrint("Text Button");
+              },
+              child: const Text("Text Button"),
+            ),
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                debugPrint("This is the row");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row Widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged: (bool newBool) {
+                setState(() {
+                  isSwitch = newBool;
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheckBox,
+              onChanged: (bool? newBool) {
+                setState(() {
+                  isCheckBox = newBool;
+                });
+              },
+            ),
+            Image.network("https://wallpaperaccess.com/full/1909531.jpg")
+          ],
+        ),
       ),
     );
   }
